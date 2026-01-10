60a8753137cfd679f06614840d0712b609e006ae
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-01-10 20:27:18 +0800

    Used axios to fetch the data

--- ecommerce/src/pages/HomePage.jsx
+++ ecommerce/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { Header } from '../components/Header';
 import './HomePage.css';
 
@@ -7,11 +8,9 @@ import { products } from '../../starting-code/data/products'
 
 export function HomePage() {
 
-    fetch('http://localhost:3000/api/products')
+    axios.get('http://localhost:3000/api/products')
         .then((response) => {
-            return response.json()
-        }).then((data) => {
-            console.log(data)
+            console.log(response.data)
         })
 
     return (
