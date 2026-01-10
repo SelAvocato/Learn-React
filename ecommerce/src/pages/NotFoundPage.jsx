1709dbf8af18bc333a2bbdd0468f0354c2c56ac3
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-01-10 19:20:46 +0800

    Added ecommerce backend

---
+++ ecommerce/src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import { Header } from '../components/Header'
+
+export function NotFoundPage(){
+    return(
+        <>
+            <Header />
+
+            <title>Page Not Found</title>
+            <h1 style={{textAlign: "center", fontSize: "100px", marginTop: "10rem"}}> PAGE NOT FOUND </h1>     
+        </>  
+    )
+}
