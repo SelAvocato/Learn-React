29bbc627cd2a4e78513943ab816114bfaeb3c3c0
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-01-13 19:06:01 +0800

    added cart in the notfoundpage

--- ecommerce/src/pages/NotFoundPage.jsx
+++ ecommerce/src/pages/NotFoundPage.jsx
@@ -1,9 +1,9 @@
 import { Header } from '../components/Header'
 
-export function NotFoundPage(){
+export function NotFoundPage({ cart }){
     return(
         <>
-            <Header />
+            <Header cart={cart}/>
 
             <title>Page Not Found</title>
             <h1 style={{textAlign: "center", fontSize: "100px", marginTop: "10rem"}}> PAGE NOT FOUND </h1>     
