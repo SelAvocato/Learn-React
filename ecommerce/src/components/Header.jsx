e91ea7b380e3a88ccf99137b48c592a7a58cec21
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-01-19 19:41:45 +0800

    Added functionality for the search input and button

--- ecommerce/src/components/Header.jsx
+++ ecommerce/src/components/Header.jsx
@@ -1,4 +1,5 @@
-import { NavLink } from 'react-router'
+import { NavLink, useNavigate } from 'react-router'
+import { useState } from 'react'
 import WhiteLogo from '../assets/images/logo-white.png'
 import MobileLogo from '../assets/images/mobile-logo-white.png'
 import CartIcon from '../assets/images/icons/cart-icon.png'
@@ -6,19 +7,41 @@ import SearchIcon from '../assets/images/icons/search-icon.png'
 
 import './Header.css';
 
-export function Header({cart}) {
+export function Header({cart, searchText}) {
+    const navigate = useNavigate()
+
     let totalQuantity = 0;
 
     cart.forEach(cartItem => {
         totalQuantity += cartItem.quantity
     })
 
+    const [search, setSearch] = useState(searchText)
+    const productName = (event) => {
+        setSearch(event.target.value)
+    }
+
+    const searchProduct = (event) => {
+        if(event.key === "Enter") {    
+            return navigate(`/?search=${search}`)
+        } else if (event.key === "Escape"){
+            return navigate(`/?search=${search}`)
+        }
+    }
+
+    const resetSearch = () => {
+        setSearch("")
+    }
+    
+    const searchBtn = () => {
+        navigate(`/?search=${search}`)
+    }
 
     return (
         <>
             <div className="header">
                 <div className="left-section">
-                    <NavLink to="/" className="header-link">
+                    <NavLink to="/" className="header-link" onClick={resetSearch}>
                         <img className="logo"
                             src={WhiteLogo} />
                         <img className="mobile-logo"
@@ -27,9 +50,9 @@ export function Header({cart}) {
                 </div>
 
                 <div className="middle-section">
-                    <input className="search-bar" type="text" placeholder="Search" />
+                    <input className="search-bar" type="text" placeholder="Search" onChange={productName} value={search} onKeyDown={searchProduct}/>
 
-                    <button className="search-button">
+                    <button className="search-button" onClick={searchBtn} >
                         <img className="search-icon" src={SearchIcon} />
                     </button>
                 </div>
--- ecommerce/src/pages/home/HomePage.jsx
+++ ecommerce/src/pages/home/HomePage.jsx
@@ -1,27 +1,30 @@
 import axios from 'axios'
+import { useSearchParams } from 'react-router';
 import { useEffect, useState } from 'react'
 import { Header } from '../../components/Header';
 import './HomePage.css';
 import { ProductsGrid } from './ProductsGrid';
 
 export function HomePage({cart, loadCart}) {
+    const [searchParams] = useSearchParams()
+    const search = searchParams.get('search')
+    
     const [products, setProducts] = useState([]);
-
     useEffect(() => {
         const getHomeData = async () => {
-            const response = await axios.get('/api/products')
+            const response = search != null ? await axios.get(`/api/products?search=${search}`) : await axios.get(`/api/products`) 
             setProducts(response.data)
         }
 
         getHomeData()
-    }, [])
+    }, [search])
 
     return (
         <>
             <link rel="icon" href="home-favicon.png" />
             <title>Ecommerce Project</title>
 
-            <Header cart={cart} />
+            <Header cart={cart} searchText={search} />
             <div className="home-page">
                 <ProductsGrid products={products} loadCart={loadCart} />
             </div>
