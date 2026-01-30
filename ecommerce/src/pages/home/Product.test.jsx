0abd42dc7e9203ea8643d5adb22478c2adb13713
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-01-31 00:42:29 +0800

    Tested for axios.post after changing the quantity

--- ecommerce/src/pages/home/Product.test.jsx
+++ ecommerce/src/pages/home/Product.test.jsx
@@ -74,5 +74,16 @@ describe('Product Component', () => {
 
         expect(quantitySelector).toHaveValue('1')
         user.selectOptions(quantitySelector, '3')
+        
+        const addToCartButton = screen.getByTestId('add-to-cart-button')
+        await user.click(addToCartButton)
+        
+        expect(axios.post).toHaveBeenCalledWith('/api/cart-items', {
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 3
+        })
+
+        expect(loadCart).toHaveBeenCalled();
+
     })
 })
